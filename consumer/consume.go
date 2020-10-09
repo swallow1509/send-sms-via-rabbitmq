fb9c3cfc6b93d1bd0ce485386c4f9adf68ca84f5
swallow1509 <umidbek.yusupov.98@mail.ru>
2020-10-09 21:35:49 +0500

    Updated consumer

--- consumer/consume.go
+++ consumer/consume.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"time"
 
 	"github.com/streadway/amqp"
 )
 
 var (
-	amqpURI  = "amqp://guest:guest@localhost:5672/"
-	conn     *amqp.Connection
-	channel  *amqp.Channel
-	newQueue amqp.Queue
-	err      error
+	amqpURI   = "amqp://guest:guest@localhost:5672/"
+	conn      *amqp.Connection
+	channel   *amqp.Channel
+	taskQueue amqp.Queue
+	err       error
 )
 
 func failOnError(err error, msg string) {
@@ -29,24 +31,31 @@ func main() {
 	failOnError(err, "Failed to open a channel(consumer)")
 	defer channel.Close()
 
-	newQueue, err = channel.QueueDeclare(
-		"newQueue",
-		false,
+	taskQueue, err = channel.QueueDeclare(
+		"task", //name
+		true,   //durable
 		false,
 		false,
 		false,
 		nil,
 	)
-	failOnError(err, "Failed to declare a queue(consumer)")
+	failOnError(err, "Failed to declare a queue")
+
+	err = channel.Qos(
+		1, //fetch count
+		0, //preFetch size
+		false,
+	)
+	failOnError(err, "Failed to set Qos!")
 
 	messages, err := channel.Consume(
-		newQueue.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
+		taskQueue.Name, //queue
+		"",             // consumer
+		false,          // auto-ack
+		false,          // exclusive
+		false,          // no-local
+		false,          // no-wait
+		nil,            // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
@@ -55,6 +64,11 @@ func main() {
 	go func() {
 		for d := range messages {
 			log.Printf(" [x] Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false)
 		}
 	}()
 
